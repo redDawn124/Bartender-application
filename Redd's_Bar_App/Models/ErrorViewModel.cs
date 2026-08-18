4e37df7f4246c9a222e5a3a541451a2f75bc8a8c
Robert Stanier <150394260+redDawn124@users.noreply.github.com>
2026-08-18 17:09:46 -0400

    Setup basic MVC Layout for project

---
+++ Redd's_Bar_App/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Redd_s_Bar_App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
